57ca13ae0c310a854ecc5756b377398c91f80f01
alif <alifadiawan2005@gmail.com>
2023-12-01 22:02:36 +0700

    awal2

---
+++ resources/views/home.blade.php
@@ -0,0 +1,61 @@
+@extends('layout.body')
+@section('content')
+    {{-- hero --}}
+    <div class="hero">
+        <img src="{{ asset('assets\hero.png') }}" style="width: 100%; height: 35rem; object-fit: cover; position: relative;">
+
+        <img src="{{ asset('assets\logo.png') }}" alt="logo" width="150" height="150"
+            style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)">
+        <h4 class="text-light" style="position: absolute; top: 63%; left: 50%; transform: translate(-50%, -50%)">Yayasan
+            An-nur</h4>
+    </div>
+
+
+    {{-- content --}}
+    <div class="container" style="margin-top: 15rem;">
+        <div class="d-flex flex-column text-center my-5">
+            <div class="col">
+                <h1>Yayasan An-nur</h1>
+            </div>
+            <div class="col">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae et voluptatem mollitia facilis nulla aliquid
+                ea sunt quasi ut at odit corporis eos necessitatibus, corrupti eaque iure distinctio optio blanditiis! Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Dolores facilis expedita porro fugit magni debitis beatae
+                est quod, quos itaque sed sit ducimus delectus unde, veniam tenetur aspernatur ipsum quibusdam! Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Veritatis, incidunt tempora provident laborum doloribus
+                reiciendis iste amet voluptatum nemo deserunt sint est esse ratione, deleniti consectetur iusto laudantium
+                in non! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur vitae quo impedit sapiente? Nam
+                fuga illo provident! Officia sapiente consequuntur at assumenda, harum, aperiam tenetur porro voluptatum ad
+                facere reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quod aut, recusandae
+                alias inventore accusantium eligendi earum. Vero doloremque incidunt magnam saepe alias repellendus nobis,
+                rerum, adipisci laboriosam itaque aperiam.
+            </div>
+        </div>
+        <div class="d-flex flex-column text-center my-5">
+            <div class="col">
+                <h1>Yayasan An-nur</h1>
+            </div>
+            <div class="col">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae et voluptatem mollitia facilis nulla aliquid
+                ea sunt quasi ut at odit corporis eos necessitatibus, corrupti eaque iure distinctio optio blanditiis! Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Dolores facilis expedita porro fugit magni debitis beatae
+                est quod, quos itaque sed sit ducimus delectus unde, veniam tenetur aspernatur ipsum quibusdam! Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Veritatis, incidunt tempora provident laborum doloribus
+                reiciendis iste amet voluptatum nemo deserunt sint est esse ratione, deleniti consectetur iusto laudantium
+                in non! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur vitae quo impedit sapiente? Nam
+                fuga illo provident! Officia sapiente consequuntur at assumenda, harum, aperiam tenetur porro voluptatum ad
+                facere reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quod aut, recusandae
+                alias inventore accusantium eligendi earum. Vero doloremque incidunt magnam saepe alias repellendus nobis,
+                rerum, adipisci laboriosam itaque aperiam.
+            </div>
+        </div>
+        <div class="d-flex flex-column text-center my-5">
+            <div class="col">
+                <h1>Program An-Nur</h1>
+            </div>
+            <div class="col">
+                
+            </div>
+        </div>
+    </div>
+@endsection

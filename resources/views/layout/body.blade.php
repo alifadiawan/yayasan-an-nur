90e537295f6ecb6d3c48a2f50e9ebe2be294e552
alif <alifadiawan2005@gmail.com>
2023-12-03 22:35:29 +0700

    carousel program

--- resources/views/layout/body.blade.php
+++ resources/views/layout/body.blade.php
@@ -9,7 +9,7 @@
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <link rel="stylesheet" href="{{ asset('assets/style.css') }}">
-    <title>Yayasan An-Nur</title>
+    <title>Yayasan An-Nur @yield('content-title')</title>
 </head>
 
 <body>

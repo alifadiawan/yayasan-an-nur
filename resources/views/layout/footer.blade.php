84d031b5ebd7cc7d38dc62219afc76c10aba6abf
alif <alifadiawan2005@gmail.com>
2024-01-21 12:14:03 +0700

    fix footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,50 +1,46 @@
-<div class="container">
-    <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+<div class="container-fluid bg-success text-white">
+    <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 justify-content-center py-5 bg-success border-top">
       <div class="col mb-3">
         <a href="/" class="d-flex justify-content-center align-items-center mb-3 link-body-emphasis text-decoration-none">
           <img class="bi me-2" src="{{ asset('assets/logo.png') }}" width="200" height="200">
         </a>
         <div class="d-flex flex-row justify-content-center">
-            <a href="" class="btn"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
-            <a href="" class="btn"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-            <a href="" class="btn"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+            <a href=""  class="btn text-white"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+            <a href=""  class="btn text-white"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href=""  class="btn text-white"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
         </div>
       </div>
   
       <div class="col mb-3">
-  
-      </div>
-  
-      <div class="col mb-3">
         <h5>Section</h5>
         <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+          <li class="nav-item mb-2 "><a href="#" class="nav-link p-0 text-white">Home</a></li>
+          <li class="nav-item mb-2 "><a href="#" class="nav-link p-0 text-white">Features</a></li>
+          <li class="nav-item mb-2 "><a href="#" class="nav-link p-0 text-white">Pricing</a></li>
+          <li class="nav-item mb-2 "><a href="#" class="nav-link p-0 text-white">FAQs</a></li>
+          <li class="nav-item mb-2 "><a href="#" class="nav-link p-0 text-white">About</a></li>
         </ul>
       </div>
   
       <div class="col mb-3">
         <h5>Section</h5>
         <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Features</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Pricing</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">FAQs</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">About</a></li>
         </ul>
       </div>
   
       <div class="col mb-3">
         <h5>Section</h5>
         <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Features</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Pricing</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">FAQs</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">About</a></li>
         </ul>
       </div>
     </footer>

0347cb45d1a02c56f3281a0757973937ad55e973
alif <alifadiawan2005@gmail.com>
2024-01-21 12:07:28 +0700

    add footer

--- resources/views/layout/body.blade.php
+++ resources/views/layout/body.blade.php
@@ -20,10 +20,14 @@
     {{-- content --}}
     @yield('content')
 
+    {{-- footer --}}
+    @include('layout.footer')
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
 </body>
 
 </html>
